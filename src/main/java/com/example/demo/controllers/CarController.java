1588d3a0d8ee675366cf4fa77a7606455faee552
Erik <59744487+knottem@users.noreply.github.com>
2023-10-04 14:31:00 +0200

    added both integration and system tests for calculator

--- src/main/java/com/example/demo/controllers/CarController.java
+++ src/main/java/com/example/demo/controllers/CarController.java
@@ -10,14 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CarController {
 
-    Calculator calculator = new Calculator();
-
     @GetMapping("/car")
     public Car getCar(){
         return new Car();
     }
-    @GetMapping("/calculate/{number1}/{number2}/{operator}")
-    public int getCalculate(@PathVariable int number1, @PathVariable int number2, @PathVariable String operator){
-        return calculator.calculate(number1, number2, operator);
-    }
+
 }
