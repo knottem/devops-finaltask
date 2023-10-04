615ccfd3418752235823143539f1fc7321619bbd
Erik <59744487+knottem@users.noreply.github.com>
2023-10-04 13:47:30 +0200

    added start of a new endpoint that we will start working on in another branch

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Calculator {
+    public int calculate(int number1, int number2, String operator) {
+        return 0;
+    }
+}
--- src/main/java/com/example/demo/controllers/CarController.java
+++ src/main/java/com/example/demo/controllers/CarController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controllers;
 
+import com.example.demo.Calculator;
 import com.example.demo.Car;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
 
+    Calculator calculator = new Calculator();
+
     @GetMapping("/car")
     public Car getCar(){
         return new Car();
+    }
+    @GetMapping("/calculate/{number1}/{number2}/{operator}")
+    public int getCalculate(@PathVariable int number1, @PathVariable int number2, @PathVariable String operator){
+        return calculator.calculate(number1, number2, operator);
     }
 }
